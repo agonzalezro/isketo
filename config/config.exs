761ed6657d2c237775fb276d813368732f8253c0
Álex González <agonzalezro@gmail.com>
2021-09-04 23:11:22 +0200

    Enable CookieBot with config

--- config/config.exs
+++ config/config.exs
@@ -27,6 +27,10 @@ config :phoenix, :json_library, Jason
 
 config :isketo, :scraper_http_adapter, Isketo.Scraper.Http.Client
 
+config :isketo, :cookiebot,
+  enabled: false,
+  id: "1105dd25-7423-42cf-9362-3d2b9ca4a80b"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
