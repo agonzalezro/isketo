fe4531beae29fa437a5762c412faac2c63b32a64
Álex González <agonzalezro@gmail.com>
2021-09-05 10:24:41 +0200

    HTTP to HTTPs on prod

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :isketo, IsketoWeb.Endpoint,
   url: [host: "isketo.com", port: 443, scheme: "https"],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto], hsts: true, host: nil]
 
 # Do not print debug messages in production
 config :logger, level: :info
